1ee8c89d4eba557dd33f043d63cb80e50b5bf96b
madhukar <phatak.dev@gmail.com>
2014-08-10 20:47:25 +0530

    videoplayback implemented

--- src/main/scala/com/madhu/hellomoon/VideoViewActivity.scala
+++ src/main/scala/com/madhu/hellomoon/VideoViewActivity.scala
@@ -2,32 +2,38 @@ package com.madhu.hellomoon
 
 
 import android.support.v4.app._
-import android.widget.{VideoView,LinearLayout}
+import android.widget.{MediaController, VideoView, LinearLayout}
 import android.net.Uri
 import android.os.Bundle
 
 import macroid._
-import macroid.Ui
 import macroid.FullDsl._
 import macroid.contrib.LpTweaks._
+import android.app.Activity
 
-class VideoViewActivity extends FragmentActivity with 
-Contexts[FragmentActivity] with IdGeneration{
+class VideoViewActivity extends Activity with
+Contexts[Activity] with IdGeneration{
 
- val url = Uri.parse("android:resource://"+"com.madhu.hellomoon/"+
- 	"raw/sample_mpeg4")
+ val url = Uri.parse("android.resource://com.madhu.hellomoon/raw/small")
 
+  var video = slot[VideoView]
   override def onCreate(savedInstanceState: Bundle) = {  
     super.onCreate(savedInstanceState)
-    val view = l[LinearLayout] {	
-      w[VideoView] <~ matchParent <~
+    val view = w[VideoView] <~ matchParent <~
       Tweak { (
       (view:VideoView) => {
       	view.setVideoURI(url)
-      	view.start()
+        val mediaController = new MediaController(this)
+        mediaController.setAnchorView(view)
+        view.setMediaController(mediaController)
+        view.start()
       }
     )}	
-    } <~ matchParent 
+
     setContentView(getUi{view})
+
   }
+
+
+
 }
